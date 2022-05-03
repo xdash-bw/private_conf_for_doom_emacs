ee61f8918b8e65211f9d3cb8b7545d9ba8e60568
xdash-bw <xdashproject@gmail.com>
2022-05-04 00:50:04 +0800

    update

--- config.el
+++ config.el
@@ -21,9 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Hack" :size 15)
-     ;; doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13)
-     )
+
+(setq doom-font (font-spec :family "Fira Code" :weight 'bold :size 15))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -33,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'modus-operandi)
+(setq doom-theme 'doom-dracula)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -76,14 +75,30 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+
+;; ------------------------------------------
+;; global On/Off
+;; ------------------------------------------
+(setq evil-escape-key-sequence "fd")
+(menu-bar-mode 1)
+(tool-bar-mode 1)
+(treemacs-project-follow-mode 1)
+
+
 ;; ============================================================================================================
 ;; 定义 global Map
 ;; ============================================================================================================
-
 (defun vsplit-scratch ()
   (interactive)
   (+evil/window-vsplit-and-follow)
   (doom/switch-to-scratch-buffer)
+  )
+
+(defun vsplit-project-scratch ()
+  (interactive)
+  (+evil/window-vsplit-and-follow)
+  ;; (doom/switch-to-scratch-buffer)
+  (doom/switch-to-project-scratch-buffer)
   )
 
 (global-set-key (kbd "M-H") nil)
@@ -104,7 +119,6 @@
 (global-set-key (kbd "M-N") nil)
 (global-set-key (kbd "M-)") nil)
 
-
 (global-set-key (kbd "C-c h") 'evil-window-left)
 (global-set-key (kbd "C-c j") 'evil-window-down)
 (global-set-key (kbd "C-c k") 'evil-window-up)
@@ -112,7 +126,7 @@
 
 (global-set-key (kbd "M-Y") 'yank)
 
-
+(global-set-key (kbd "M-h") (kbd "C-@"))
 
 (global-set-key (kbd "C-1") 'doom/open-scratch-buffer)
 (global-set-key (kbd "C-2") 'vsplit-scratch)
@@ -123,32 +137,73 @@
 
 (global-set-key (kbd "C-9") 'persp-switch-to-buffer)
 
-;; (global-set-key (kbd "C-w") nil)
-;; (global-set-key (kbd "<backspace>") 'fuckyou)
 
-
+;;------------------------------------------------------------
+;; evil keymap
+;;------------------------------------------------------------
 (map! :leader
       "hy" #'set-frame-font
+
+      ;;---------------------------------------------
+      ;; window operator
+      ;;---------------------------------------------
       "w;" #'vsplit-scratch
+      "w:" #'vsplit-project-scratch
+      "w[" #'+evil/window-vsplit-and-follow
+
+
+      ;;---------------------------------------------
+      ;; other ui
+      ;;---------------------------------------------
       "k1" #'youdao-dictionary-search-at-point+
       "k2" #'youdao-dictionary-search-at-point
       "k3" #'eaf-open-browser-with-history
       "k4" #'eaf-open-office
-      "w[" #'+evil/window-vsplit-and-follow
 
-      "kd" #'counsel-etags-find-tag-at-point
+
+      ;;---------------------------------------------
+      ;; counsel gtags
+      ;;---------------------------------------------
+      "kc" #'counsel-gtags-create-tags
+      "ku" #'citre-global-update-database
+      "kd" #'counsel-gtags-find-definition
+      "kD" #'counsel-gtags-find-definition-other-window
+      "kg" #'counsel-gtags-find-reference
+      "kG" #'counsel-gtags-find-reference-other-window
+      "kf" #'counsel-gtags-find-file
+      "kF" #'counsel-gtags-find-file-other-windw
+      "k[" #'counsel-gtags-go-backward
+      "k]" #'counsel-gtags-go-forward
+
+
+      ;;---------------------------------------------
+      ;; counsel etags
+      ;;---------------------------------------------
+      "ke" #'counsel-etags-find-tag-at-point
+      "kE" #'counsel-etags-find-tag
       "ks" #'counsel-etags-grep
+      "kS" #'counsel-etags-grep-current-directory
       "ki" #'counsel-etags-list-tag-in-current-file
       "kI" #'counsel-etags-list-tag
 
+
+      ;;---------------------------------------------
+      ;; other
+      ;;---------------------------------------------
       "pI" #'projectile-ibuffer
+      "hh" #'treemacs
+
+
+      ;;---------------------------------------------
+      ;; lsp
+      ;;---------------------------------------------
+      "ll" #'lsp
       )
 
 
 ;; ============================================================================================================
-;; 开始加载 package
+;; use package
 ;; ============================================================================================================
-
 (use-package! rime
   :custom
   (default-input-method "rime")
@@ -162,7 +217,7 @@
      ;; 在英文字符串之后（必须为以字母开头的英文字符串）
      rime-predicate-after-alphabet-char-p
      ;; 任意英文字符后
-     ;; rime-predicate-after-ascii-char-p
+     rime-predicate-after-ascii-char-p
      ;; 在中文字符且有空格之后
      rime-predicate-space-after-cc-p
      ;; 将要输入的为大写字母时
@@ -170,80 +225,14 @@
      ))
   )
 
-;; (add-to-list 'load-path "/home/jianan-xie/.emacs.d/site-lisp/emacs-application-framework")
-;; (require 'eaf)
-;; (require 'eaf-image-viewer)
-;; ;; (require 'eaf-mermaid)
-;; (require 'eaf-terminal)
-;; (require 'eaf-file-manager)
-;; (require 'eaf-camera)
-;; (require 'eaf-airshare)
-;; (require 'eaf-music-player)
-;; (require 'eaf-vue-demo)
-;; (require 'eaf-rss-reader)
-;; (require 'eaf-git)
-;; (require 'eaf-demo)
-;; (require 'eaf-system-monitor)
-;; (require 'eaf-netease-cloud-music)
-;; (require 'eaf-mindmap)
-;; (require 'eaf-org-previewer)
-;; (require 'eaf-file-sender)
-;; (require 'eaf-pdf-viewer)
-;; (require 'eaf-jupyter)
-;; (require 'eaf-markdown-previewer)
-;; (require 'eaf-file-browser)
-;; (require 'eaf-video-player)
-;; (require 'eaf-browser)
-
-;; (use-package! eaf
-;;   :load-path "~/.emacs.d/site-lisp/emacs-application-framework"
-;;   :custom
-;;   ; See https://github.com/emacs-eaf/emacs-application-framework/wiki/Customization
-;; (setq eaf-enable-debug t)
-;;   (eaf-browser-continue-where-left-off t)
-;;   (eaf-browser-enable-adblocker t)
-;;   (browse-url-browser-function 'eaf-open-browser)
-;;   :config
-;;   (defalias 'browse-web #'eaf-open-browser)
-;;   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
-;;   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
-;;   (eaf-bind-key nil "M-q" eaf-browser-keybinding)
-;;   ) ;; unbind, see more in the Wiki
-
-(eval-after-load "evil"
-  '(progn
-     (defvar last-focus-buffer nil
-       "Buffer currently in focus.")
-
-     (defun buffer-focus-handler ()
-       (interactive)
-       (when (not (buffer-live-p last-focus-buffer))
-         (setq last-focus-buffer nil))
-       (when (and (eq (window-buffer (selected-window))
-                      (current-buffer))
-                  (not (eq last-focus-buffer (current-buffer))))
-         (setq last-focus-buffer (current-buffer))
-         (when (derived-mode-p 'eaf-mode)
-           (evil-insert-state))))
-
-     (defun eaf-other-histoy-other-window ()
-       (interactive)
-       (+evil/window-vsplit-and-follow)
-       (eaf-open-browser-with-history)
-       )
-
-
-     (add-hook 'buffer-list-update-hook #'buffer-focus-handler)))
-
-;;; 具体参考 mode-line-mule-info 默认值，其中可能有其它有用信息
-(setq mode-line-mule-info '((:eval (rime-lighter))))
-
 (use-package! youdao-dictionary)
-
 (use-package! counsel-etags)
+(use-package! counsel-gtags)
 
 
-
+;;------------------------------------------------------------------
+;; eaf
+;;------------------------------------------------------------------
 (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
 (require 'eaf)
 (require 'eaf-browser)
@@ -273,32 +262,30 @@
   ;; (eaf-bind-key nil "M-q" eaf-browser-keybinding)
   ) ;; unbind, see more in the Wiki
 
+(eval-after-load "evil"
+  '(progn
+     (defvar last-focus-buffer nil
+       "Buffer currently in focus.")
+
+     (defun buffer-focus-handler ()
+       (interactive)
+       (when (not (buffer-live-p last-focus-buffer))
+         (setq last-focus-buffer nil))
+       (when (and (eq (window-buffer (selected-window))
+                      (current-buffer))
+                  (not (eq last-focus-buffer (current-buffer))))
+         (setq last-focus-buffer (current-buffer))
+         (when (derived-mode-p 'eaf-mode)
+           (evil-insert-state))))
+
+     (defun eaf-other-histoy-other-window ()
+       (interactive)
+       (+evil/window-vsplit-and-follow)
+       (eaf-open-browser-with-history)
+       )
 
 
-;; lsp and gtags and ctags
-(map! :leader
-      "ll" #'lsp
-      )
+     (add-hook 'buffer-list-update-hook #'buffer-focus-handler)))
 
-
-;; (use-package! lsp-jedi
-;;   :ensure t
-;;   :config
-;;   (with-eval-after-load "lsp-mode"
-;;   (add-to-list 'lsp-disabled-clients 'pyls)
-;;   (add-to-list 'lsp-enabled-clients 'jedi)))
-
-;; (setq lsp-jedi-executable-command "/home/jianan-xie/.local/bin/jedi-language-server")
-
-;; (use-package! native-complete
-;;   :ensure t
-;;   ()
-;;   )
-
-(global-set-key (kbd "M-q") nil)
-(define-prefix-command 'ring-map)
-(global-set-key (kbd "M-q") 'ring-map)
-(global-set-key (kbd "M-q h") 'evil-window-left)
-(global-set-key (kbd "M-q j") 'evil-window-down)
-(global-set-key (kbd "M-q k") 'evil-window-up)
-(global-set-key (kbd "M-q l") 'evil-window-right)
+;;; 具体参考 mode-line-mule-info 默认值，其中可能有其它有用信息
+(setq mode-line-mule-info '((:eval (rime-lighter))))
